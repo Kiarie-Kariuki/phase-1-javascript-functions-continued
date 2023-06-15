fe305f083f4b46c96c65c3c280a38d93ebbac461
Collins <collinskiarie13@outlook.com>
2023-06-16 00:40:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+function saturdayFun(activity){
+    if (activity === undefined){
+        return "This Saturday, I want to roller-skate!"
+    }else
+        return `This Saturday, I want to ${activity}!` 
+};
+saturdayFun();
+
+function mondayWork(activity){
+    if(activity === undefined){
+        return "This Monday, I will go to the office."
+    }else
+        return `This Monday, I will ${activity}.`
+};
+mondayWork(); 
+
+function wrapAdjective(flair = '*'){
+    return function(adjective = "a hard worker"){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
+console.log(wrapAdjective());
+
+function wrapAdjective(flair = '||'){
+    return function(adjective = "a dedicated programmer"){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
+console.log(wrapAdjective());
